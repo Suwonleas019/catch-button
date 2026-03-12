182a5630f559b4000c6793f8d61db8bf5b1a6a98
수환 김 <leas019@suwon.ac.kr>
2026-03-12 17:18:10 +0900

    점수 표시를 폼 화면에 나오게 수정 클릭에 성공하면 크기가 10% 작게 수

--- Form1.cs
+++ Form1.cs
@@ -5,11 +5,22 @@ namespace WinFormsApp_3
 {
     public partial class 버튼잡기게임 : Form
     {
+        private int score = 0; // 점수 저장
+        private Random rand = new Random(); // 랜덤 좌표 생성용
         public 버튼잡기게임()
         {
             InitializeComponent();
+            UpdateTitleScore(); // 시작 시 제목 표시줄 초기화
         }
-
+        private void UpdateScoreDisplay()
+        {
+            this.Text = "점수: " + score + "점 - 버튼 잡기 게임";
+        }
+        // 2. 점수를 제목 표시줄에 업데이트하는 공통 함수
+        private void UpdateTitleScore()
+        {
+            this.Text = $"현재 점수: {score}점 | 마우스를 피해 도망가는 버튼을 잡아보세요!";
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -17,6 +28,10 @@ namespace WinFormsApp_3
 
         private void target_MouseEnter(object sender, EventArgs e)
         {
+            // 점수 감점 및 업데이트
+            score -= 10;
+            UpdateTitleScore();
+
             SystemSounds.Beep.Play();
 
             Random rd = new Random();
@@ -29,17 +44,25 @@ namespace WinFormsApp_3
 
             target.Location = new Point(nextX, nextY);
 
-            this.Text = $"버튼위치: ({nextX}, {nextY})";
+            //this.Text = $"버튼위치: ({nextX}, {nextY})";
+         
         }
 
         private void target_Click(object sender, EventArgs e)
         {
+            // 점수 가점 및 업데이트
+            score += 100;
+            UpdateTitleScore();
+
+            target.Width = (int)(target.Width * 0.9);
+            target.Height = (int)(target.Height * 0.9);
+
             SystemSounds.Asterisk.Play();
 
-            MessageBox.Show("축하합니다!");
+            //MessageBox.Show("축하합니다!");
 
             
-            Application.Exit();
+            //Application.Exit();
         }
     }
 }
