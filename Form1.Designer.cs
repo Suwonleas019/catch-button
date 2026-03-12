620b66c7f28b400c49c0fba6c5ff46d4a93ff5f4
수환 김 <leas019@suwon.ac.kr>
2026-03-12 16:27:59 +0900

    버튼에 마우스를 가져다 대면 무작의 위치로 이동을 하게 된다. 하지만 버튼 크기 이상 화면 밖으로 빠져나가지 않게 만듬. 또한 버튼을 클릭했을때 성취 문구와 함께 프로그램이 종료되게 만

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp_3
 {
-    partial class Form1
+    partial class 버튼잡기게임
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            target = new Button();
+            SuspendLayout();
+            // 
+            // target
+            // 
+            target.BackColor = Color.FromArgb(255, 128, 128);
+            target.Font = new Font("맑은 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            target.ForeColor = SystemColors.ActiveCaptionText;
+            target.Location = new Point(322, 143);
+            target.Name = "target";
+            target.Size = new Size(229, 104);
+            target.TabIndex = 0;
+            target.Text = "나를 잡아봐";
+            target.UseVisualStyleBackColor = false;
+            target.Click += target_Click;
+            target.MouseEnter += target_MouseEnter;
+            // 
+            // 버튼잡기게임
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(target);
+            Name = "버튼잡기게임";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button target;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,41 @@
+using System.Collections.Specialized;
+
 namespace WinFormsApp_3
 {
-    public partial class Form1 : Form
+    public partial class 버튼잡기게임 : Form
     {
-        public Form1()
+        public 버튼잡기게임()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void target_MouseEnter(object sender, EventArgs e)
+        {
+
+            Random rd = new Random();
+
+            int maxX = this.ClientSize.Width - target.Width;
+
+            int maxY = this.ClientSize.Height - target.Height;
+
+            int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
+
+            target.Location = new Point(nextX, nextY);
+
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+        }
+
+        private void target_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("나를 잡았구나 축하해!");
+
+            
+            Application.Exit();
         }
     }
 }
