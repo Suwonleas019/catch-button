92a43acffb8d7db19a8f89d39825f57e93cf7cf6
수환 김 <leas019@suwon.ac.kr>
2026-03-12 20:58:02 +0900

    버튼을 누르면 점수가 오르로 실패하면 점수가 깎임 20번 누적 실패 시 game over 와 함께 게임 종료 후 다시시작 버튼 활성화 다시시작 버튼 누를 시 게임 재시

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             target = new Button();
+            btnRestart = new Button();
             SuspendLayout();
             // 
             // target
@@ -36,20 +37,32 @@
             target.BackColor = Color.FromArgb(255, 128, 128);
             target.Font = new Font("맑은 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
             target.ForeColor = SystemColors.ActiveCaptionText;
-            target.Location = new Point(322, 143);
+            target.Location = new Point(234, 66);
             target.Name = "target";
-            target.Size = new Size(229, 104);
+            target.Size = new Size(300, 200);
             target.TabIndex = 0;
             target.Text = "나를 잡아봐";
             target.UseVisualStyleBackColor = false;
             target.Click += target_Click;
             target.MouseEnter += target_MouseEnter;
             // 
+            // btnRestart
+            // 
+            btnRestart.Location = new Point(260, 192);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(247, 70);
+            btnRestart.TabIndex = 1;
+            btnRestart.Text = "다시시작";
+            btnRestart.UseVisualStyleBackColor = true;
+            btnRestart.Visible = false;
+            btnRestart.Click += btnRestart_Click_1;
+            // 
             // 버튼잡기게임
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnRestart);
             Controls.Add(target);
             Name = "버튼잡기게임";
             Text = "Form1";
@@ -60,5 +73,6 @@
         #endregion
 
         private Button target;
+        private Button btnRestart;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,68 +1,101 @@
+using System;
 using System.Collections.Specialized;
+using System.Drawing;
 using System.Media;
+using System.Windows.Forms;
 
 namespace WinFormsApp_3
 {
     public partial class 버튼잡기게임 : Form
     {
-        private int score = 0; // 점수 저장
-        private Random rand = new Random(); // 랜덤 좌표 생성용
+
+
         public 버튼잡기게임()
         {
             InitializeComponent();
-            UpdateTitleScore(); // 시작 시 제목 표시줄 초기화
         }
-        private void UpdateScoreDisplay()
-        {
-            this.Text = "점수: " + score + "점 - 버튼 잡기 게임";
-        }
-        // 2. 점수를 제목 표시줄에 업데이트하는 공통 함수
-        private void UpdateTitleScore()
-        {
-            this.Text = $"현재 점수: {score}점 | 마우스를 피해 도망가는 버튼을 잡아보세요!";
-        }
-        private void Form1_Load(object sender, EventArgs e)
+        int score = 0;
+        int missCount = 0; // 놓친 횟수 카운트
+        private void Form1_Load(object? sender, EventArgs e)
         {
 
         }
 
-        private void target_MouseEnter(object sender, EventArgs e)
+        private void target_MouseEnter(object? sender, EventArgs e)
         {
-            // 점수 감점 및 업데이트
-            score -= 10;
-            UpdateTitleScore();
+            missCount++; //놓친 횟수 증가
+            score -= 10; // 10점 감점
+            this.Text = "현재 점수: " + score;
+            this.Text = "놓친 횟수; " + missCount; // 폼 제목 업데이트클릭 실패 시
+            Random rand = new Random();
 
-            SystemSounds.Beep.Play();
-
-            Random rd = new Random();
-
+            // 2. 버튼이 움직일 수 있는 최대 범위 계산 (폼의 크기 - 버튼의 크기)
             int maxX = this.ClientSize.Width - target.Width;
-
             int maxY = this.ClientSize.Height - target.Height;
 
-            int nextX = rd.Next(0, maxX); int nextY = rd.Next(0, maxY);
+            // 3. 범위 내에서 무작위 좌표 생성
+            int nextX = rand.Next(0, maxX);
+            int nextY = rand.Next(0, maxY);
 
+            // 4. 버튼 위치 변경
             target.Location = new Point(nextX, nextY);
+            // 20번 놓치면 게임 오버
+            if (missCount >= 20)
+            {
+                GameOver();
+                return; // 더 이상 도망가지 않게 중단
+            }
 
-            //this.Text = $"버튼위치: ({nextX}, {nextY})";
          
         }
 
-        private void target_Click(object sender, EventArgs e)
+        // [게임 종료 처리]
+        private void GameOver()
         {
-            // 점수 가점 및 업데이트
-            score += 100;
-            UpdateTitleScore();
+            MessageBox.Show("Game Over");
+            target.Enabled = false; // 도망가는 버튼 비활성화
+            btnRestart.Visible = true;
 
+            // '다시 시작' 버튼 생성 또는 활성화
+            // (이미 폼에 다시 시작 버튼을 만들어 두었다면 btnRestart.Visible = true; 등으로 처리)
+        }
+
+        // [다시 시작 버튼 클릭 시] 초기화 로직
+       
+
+        private void target_Click(object? sender, EventArgs e)
+        {
+            score += 100; // 100점 추가 클릭 성공시
+            this.Text = "현재 점수: " + score; // 폼 제목 업데이트
             target.Width = (int)(target.Width * 0.9);
             target.Height = (int)(target.Height * 0.9);
+            Random rand = new Random();
 
-            SystemSounds.Asterisk.Play();
+            // 2. 버튼이 움직일 수 있는 최대 범위 계산 (폼의 크기 - 버튼의 크기)
+            int maxX = this.ClientSize.Width - target.Width;
+            int maxY = this.ClientSize.Height - target.Height;
 
-            //MessageBox.Show("축하합니다!");
+            // 3. 범위 내에서 무작위 좌표 생성
+            int nextX = rand.Next(0, maxX);
+            int nextY = rand.Next(0, maxY);
 
-            
-            //Application.Exit();
+            // 4. 버튼 위치 변경
+            target.Location = new Point(nextX, nextY);
+        }
+
+        private void btnRestart_Click_1(object sender, EventArgs e)
+        {
+            // 점수 및 카운트 초기화
+            score = 0;
+            missCount = 0;
+            this.Text = "점수: 0 | 놓친 횟수: 0/20";
+
+            // 버튼 복구
+            target.Enabled = true;
+            target.Width = 300;  // 처음 크기로 복구 (예시)
+            target.Height = 200;
+            target.Location = new Point(100, 100); // 처음 위치로 복구
+            btnRestart.Visible = false;
         }
     }
 }
